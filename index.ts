2c9dab32fc97d7a5e9e4c93d1ee95e73e5845e55
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-04 23:15:49 +0500

    Program to find the the sum of two number 8 & 3 by function

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-// Program to find the the sum of two number by function
+// Program to find the the sum of two number 8 & 3 by function
 
 function numSum() {
   // values
-  var num2: number = 8;
   var num1: number = 3;
+  var num2: number = 8;
 
   // sum
 
