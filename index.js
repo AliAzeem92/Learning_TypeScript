9bc0ce2b01e287d2c5a74e39c3e981501b154ac2
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-05 01:15:17 +0500

    Arrow Fuction

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 "use strict";
-// Program to find the the sum of 10, 20 by function. But the result must be "50". And add '5' after calculation
-function sum(num1, num2) {
+// Arrow Function
+var arrowFun = (num1, num2) => {
+    //   var num1: number = 2;
+    //   var num2: number = 5;
     var result = num1 + num2;
     return result;
-}
-var sumValue = sum(10, 20);
-sumValue += 5;
-console.log("Sum is", sumValue);
-// Program to find the the sum of 30, 20 by function. But the result must be "50". And add '40' after calculation
-var sumValue2 = sum(30, 20);
-sumValue2 += 40;
-console.log("sum is", sumValue2);
+    console.log(result);
+};
+var sunArrow = arrowFun(2, 5);
+sunArrow += 3;
+console.log(sunArrow);
--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
-// Program to find the the sum of 10, 20 by function. But the result must be "50". And add '5' after calculation
+// Arrow Function
 
-function sum(num1: number, num2: number) {
+var arrowFun = (num1: number, num2: number) => {
+  //   var num1: number = 2;
+  //   var num2: number = 5;
   var result: number = num1 + num2;
   return result;
-}
+  console.log(result);
+};
 
-var sumValue: number = sum(10, 20);
-sumValue += 5;
-console.log("Sum is", sumValue);
-
-// Program to find the the sum of 30, 20 by function. But the result must be "50". And add '40' after calculation
-
-var sumValue2: number = sum(30, 20);
-sumValue2 += 40;
-console.log("sum is", sumValue2);
+var sunArrow: number = arrowFun(2, 5);
+sunArrow += 3;
+console.log(sunArrow);
