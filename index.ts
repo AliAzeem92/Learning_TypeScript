ef83c5c921abe0a9ea655d1b5d8ad3f30383d12c
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-05 01:58:38 +0500

    Concept of 'var, let & const'(End of the Day 04)

--- index.ts
+++ index.ts
@@ -1,6 +1,45 @@
-// Arrow Function
+// Concept of "Var Let Const"
 
-var arrowFun = (num1: number, num2: number) => num1 + num2;
-var sunArrow: number = arrowFun(2, 5);
-sunArrow += 3;
-console.log(sunArrow);
+// Value of "var & let" can be change. But the value of "const" cannot be changed.
+
+var valueOfVar: number = 23;
+valueOfVar = 50;
+console.log(valueOfVar);
+
+let valueOfLet: string = "Hello";
+valueOfLet = "hi";
+console.log(valueOfLet);
+
+const valueOfConst: number = 29;
+// valueOfConst = 30;(Shows error)
+
+//"All of them are not accessable out side the funtion"
+
+function checking() {
+  var varFun: number = 20;
+  let letFun: number = 30;
+  const constFun: number = 40;
+}
+
+// console.log(varFun);
+// console.log(letFun);
+// console.log(constFun);
+// (Shows error)
+
+// var = 50
+// let = 60
+// const = 70
+// (Shows error)
+
+// "Var is accessable out side any block(Like if statement block) if it's not fuction funtion block but 'Let & Const' are still not accessable"
+
+{
+  var varF: string = "Var";
+  let letF: string = "Let";
+  const constF: string = "Const";
+}
+
+console.log(varF);
+// console.log(letF);
+// console.log(constF);
+// Shows error
