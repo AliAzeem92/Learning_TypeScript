10b299020040c39b2a506bb74b9017efab023f53
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-04 23:24:26 +0500

    Program to find the the sum of two by function and call it 3 times. But the result must be different.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-// Program to find the the sum of two by function. But the result must be "50"
+// Program to find the the sum of two by function and call it 3 times. But the result must be different.
 function numSum(num1, num2) {
     // sum
     var result = num1 + num2;
@@ -8,3 +8,5 @@ function numSum(num1, num2) {
 }
 // function Call
 numSum(10, 40);
+numSum(92, 60);
+numSum(32, 92);
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-// Program to find the the sum of two by function. But the result must be "50"
+// Program to find the the sum of two by function and call it 3 times. But the result must be different.
 
 function numSum(num1: number, num2: number) {
   // sum
@@ -12,3 +12,7 @@ function numSum(num1: number, num2: number) {
 // function Call
 
 numSum(10, 40);
+
+numSum(92, 60);
+
+numSum(32, 92);
