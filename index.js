23ffd93c5b67cad80d8783e844d57d07c9285bd0
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-04 23:50:52 +0500

    Program to convert temprature from Celsius to Fahranheit  using Function.

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 "use strict";
-// Program to find the the sum of two by function and call it 3 times. But the result must be different.
-function numSum(num1, num2) {
-    // sum
-    var result = num1 + num2;
-    // show
-    console.log("result is", result);
+// Program to convert temprature from Celsius to Fahranheit  using Function.
+function cToFTemp(temp) {
+    const tempF = temp * (9 / 5) + 32;
+    console.log(tempF);
 }
-// function Call
-numSum(10, 40);
-numSum(92, 60);
-numSum(32, 92);
+cToFTemp(67);
+cToFTemp(832);
+cToFTemp(72);
--- index.ts
+++ index.ts
@@ -1,18 +1,13 @@
-// Program to find the the sum of two by function and call it 3 times. But the result must be different.
+// Program to convert temprature from Celsius to Fahranheit  using Function.
 
-function numSum(num1: number, num2: number) {
-  // sum
+function cToFTemp(temp: number) {
+  const tempF = temp * (9 / 5) + 32;
 
-  var result: number = num1 + num2;
-
-  // show
-  console.log("result is", result);
+  console.log(tempF);
 }
 
-// function Call
+cToFTemp(67);
 
-numSum(10, 40);
+cToFTemp(832);
 
-numSum(92, 60);
-
-numSum(32, 92);
+cToFTemp(72);
