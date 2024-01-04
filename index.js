f546398a3147c3cb3b27a269a9fa2d4f424466d7
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-04 22:21:09 +0500

    Check Given Number is +ve, -ve or zero

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 "use strict";
-// Program to find "area of rectangle"
-const lenght = 7;
-const width = 6;
-const areaOfRectangle = lenght * width;
-console.log(areaOfRectangle);
+// Check Given Number is +ve, -ve or zero
+var num1 = 0;
+if (num1 > 0) {
+    console.log(`${num1} is Positive`);
+}
+else if (num1 < 0) {
+    console.log(`${num1} is Negative`);
+}
+else if (num1 == 0) {
+    console.log(`${num1} is Zero`);
+}
--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
-// Program to find "area of rectangle"
+// Check Given Number is +ve, -ve or zero
 
-const lenght = 7;
-const width = 6;
+var num1: number = 0;
 
-const areaOfRectangle = lenght * width;
-
-console.log(areaOfRectangle);
+if (num1 > 0) {
+  console.log(`${num1} is Positive`);
+} else if (num1 < 0) {
+  console.log(`${num1} is Negative`);
+} else if (num1 == 0) {
+  console.log(`${num1} is Zero`);
+}
