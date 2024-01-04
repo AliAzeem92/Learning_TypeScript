6fcfa0eedcc4b4c55de202fda4b4ad4c8b27acfd
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-05 01:17:06 +0500

    Arrow Function (Without typing return)

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 "use strict";
 // Arrow Function
-var arrowFun = (num1, num2) => {
-    //   var num1: number = 2;
-    //   var num2: number = 5;
-    var result = num1 + num2;
-    return result;
-    console.log(result);
-};
+var arrowFun = (num1, num2) => num1 + num2;
 var sunArrow = arrowFun(2, 5);
 sunArrow += 3;
 console.log(sunArrow);
--- index.ts
+++ index.ts
@@ -1,13 +1,6 @@
 // Arrow Function
 
-var arrowFun = (num1: number, num2: number) => {
-  //   var num1: number = 2;
-  //   var num2: number = 5;
-  var result: number = num1 + num2;
-  return result;
-  console.log(result);
-};
-
+var arrowFun = (num1: number, num2: number) => num1 + num2;
 var sunArrow: number = arrowFun(2, 5);
 sunArrow += 3;
 console.log(sunArrow);
