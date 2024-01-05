de4877154ded9dcd0d69448f6844c15833e8d808
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-05 22:29:59 +0500

    For Loop

--- loop.js
+++ loop.js
@@ -1,9 +1,5 @@
 "use strict";
-// while Loop
-// Write program that prints the table of 2
-var num = 1;
-while (num <= 10) {
-    var mutliply = num * 5;
-    console.log("5 * " + num + " = " + mutliply);
-    num += 1;
+// For Loop
+for (var num = 1; num <= 10; num++) {
+    console.log(num);
 }
--- loop.ts
+++ loop.ts
@@ -1,10 +1,5 @@
-// while Loop
+// For Loop
 
-// Write program that prints the table of 5
-var num: number = 1;
-
-while (num <= 10) {
-  var mutliply: number = num * 5;
-  console.log("5 * " + num + " = " + mutliply);
-  num += 1;
+for (var num = 1; num <= 10; num++) {
+  console.log(num);
 }
