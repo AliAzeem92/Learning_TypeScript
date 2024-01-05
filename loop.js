729ff56fe009fc408d1a6fb3296a96748c399d31
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-05 20:21:53 +0500

    While Loop Program

--- loop.js
+++ loop.js
@@ -5,3 +5,11 @@ while (num < 5) {
     console.log(num, "Hello");
     num += 1;
 }
+// Write program that prints the table of 2
+console.log("Program");
+var num = 1;
+while (num <= 10) {
+    let result = num * 2;
+    console.log("2 * " + num + " = " + result);
+    num += 1;
+}
--- loop.ts
+++ loop.ts
@@ -6,3 +6,14 @@ while (num < 5) {
   console.log(num, "Hello");
   num += 1;
 }
+
+// Write program that prints the table of 2
+console.log("Program");
+
+var num: number = 1;
+
+while (num <= 10) {
+  let result = num * 2;
+  console.log("2 * " + num + " = " + result);
+  num += 1;
+}
