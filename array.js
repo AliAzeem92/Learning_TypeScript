8129306bc5c5c2ce78a52f44e0b27df5458f934e
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-05 17:53:46 +0500

    (Day 5 Started) Array

---
+++ array.js
@@ -0,0 +1,12 @@
+"use strict";
+var studentRollNo = [1, 2, 3, 4, 5];
+console.log(studentRollNo[2]);
+var sum = studentRollNo[2] + studentRollNo[1];
+console.log(sum);
+studentRollNo[0] = 0;
+var sum2 = studentRollNo[0] + studentRollNo[1];
+console.log(sum2);
+var studentName = ["Ali", "Zain", "Raheeq", "Zair"];
+console.log(studentName[3]);
+studentName[3] = "Ahmad";
+console.log(studentName[3]);
---
+++ array.ts
@@ -0,0 +1,20 @@
+var studentRollNo: number[] = [1, 2, 3, 4, 5];
+
+console.log(studentRollNo[2]);
+
+var sum: number = studentRollNo[2] + studentRollNo[1];
+
+console.log(sum);
+
+studentRollNo[0] = 0;
+
+var sum2: number = studentRollNo[0] + studentRollNo[1];
+
+console.log(sum2);
+
+var studentName: string[] = ["Ali", "Zain", "Raheeq", "Zair"];
+
+console.log(studentName[3]);
+
+studentName[3] = "Ahmad";
+console.log(studentName[3]);
