8f3d0aab74266c3bd2d4f761c85c30669b0273dd
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-06 00:03:02 +0500

    Day 05 Ended

--- program.js
+++ program.js
@@ -1,12 +1,2 @@
 "use strict";
-// Write a program which calculate sum of even numbers from 0-100 & make lish of even numbers and print with sum.
-var num = 0;
-var list = [];
-for (let index = 0; index <= 1000; index++) {
-    if (index % 2 == 0) {
-        num = num + index;
-        list.push(index);
-    }
-}
-console.log(num);
-console.log(list);
+console.log("Day 05 Ended");
--- program.ts
+++ program.ts
@@ -1,11 +1,2 @@
-// Write a program which calculate sum of even numbers from 0-100 & make lish of even numbers and print with sum.
-var num: number = 0;
-var list: number[] = [];
-for (let index = 0; index <= 100; index++) {
-  if (index % 2 == 0) {
-    num = num + index;
-    list.push(index);
-  }
-}
-console.log(num);
-console.log(list);
+console.log("Day 05 Ended");
+console.log("Day 05 Ended");
