6f7698063b6be940938dab3727090024cee6b4d7
Ali Azeem <aliazeemaliazeem786@gmail.com>
2024-01-06 11:25:38 +0500

    3 Tasks

--- task.js
+++ task.js
@@ -1,9 +1,19 @@
 "use strict";
 function calculateStatistics(numbers) {
     // Calculate and print the sum of the numbers
-    var sum = numArray[0] + numArray[1] + numArray[2] + numArray[3] + numArray[4];
+    var sum = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
     console.log(sum);
+    // Calculate and print the average of the numbers
+    console.log(sum / numbers.length);
+    // Identify and print the largest number
+    var max = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    console.log("Largest number:", max);
 }
 // Example usage:
-const numArray = [10, 5, 8, 20, 3];
+const numArray = [10, 50, 8, 20, 3];
 calculateStatistics(numArray);
--- task.ts
+++ task.ts
@@ -1,8 +1,19 @@
 function calculateStatistics(numbers: number[]): void {
   // Calculate and print the sum of the numbers
   var sum: number =
-    numArray[0] + numArray[1] + numArray[2] + numArray[3] + numArray[4];
+    numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
   console.log(sum);
+  // Calculate and print the average of the numbers
+  console.log(sum / numbers.length);
+  // Identify and print the largest number
+  //   I have no idea how to do it
+  var max: number = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) {
+      max = numbers[i];
+    }
+  }
+  console.log("Largest number:", max);
 }
 
 // Example usage:
